07f3d4e54b89edf488525dfab588fc14b0216e28
diego silvestre borges salces <119339576+Atlaros@users.noreply.github.com>
2023-09-15 22:55:43 -0400

    Delete challenges/DevHotel-main directory

--- challenges/DevHotel-main/devhotel-backend/src/main/java/com/carlosg/devhotel/repository/BedroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carlosg.devhotel.repository;
-
-import com.carlosg.devhotel.model.Bedroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BedroomRepository extends JpaRepository<Bedroom, Long> {
-}
